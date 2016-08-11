52f1f3b19f63ca435cd4a4fd7a8a6e30c83b3346
Konstantin <konstantinseitz@live.de>
2016-08-11 17:25:33 +0200

    buttons work, added package.json for future deployment on heroku

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ var mongoose = require("mongoose");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+var options = {
+    server: {
+        socketOptions: {
+            keepAlive: 300000,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 300000,
+            connectTimeoutMS: 30000
+        }
+    }
+};
 var mongodbUri = 'mongodb://Konstantin:konstantin@ds029585.mlab.com:29585/mmdb16';
 
 
@@ -25,7 +37,7 @@ var datalist = new mongoose.Schema({
     ordernumber: Number,
     driver: String,
     time: Number,
-    date: Number,
+    date: String,
     pickup: Number,
     destination: Number,
     status: String
@@ -34,7 +46,7 @@ var datalist = new mongoose.Schema({
 var datalist = mongoose.model("datalist", datalist);
 
 
-app.post("/datalist", function(req, res){
+app.post("/datalist", function(req, res) {
     console.log(req.body);
     new datalist({
         order: req.body.order,
@@ -45,67 +57,45 @@ app.post("/datalist", function(req, res){
         pickup: req.body.pickup,
         destination: req.body.destination,
         status: req.body.status
-    }).save(function(err, doc){
+    }).save(function(err, doc) {
         res.json(doc);
     });
 });
 
-app.get("/datalist", function(req, res){
-    datalist.find({}, function(err, docs){
+app.get("/datalist", function(req, res) {
+    datalist.find({}, function(err, docs) {
         res.json(docs);
     })
 })
 
 
-
-
-app.get("/datalist", function(req, res) {
-    console.log("I received a GET request");
-    db.datalist.find(function(err, docs){
-        console.log(docs);
-        res.json(docs);
-    });
-});
-
-app.post("/datalist", function(req, res){
+app.post("/datalist", function(req, res) {
     console.log(req.body);
-    db.datalist.insert(req.body, function(err, doc){
+    db.datalist.insert(req.body, function(err, doc) {
         res.json(doc);
     });
 });
 
 
-app.delete("/datalist/:id", function(req, res){
+
+
+app.delete("/datalist/:id", function(req, res) {
     var id = req.params.id;
-    console.log(id);
-    db.datalist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
-        res.json(doc);
-    });
+    datalist.findByIdAndRemove({
+            _id: id
+        },
+        function(err, doc) {
+            res.json(doc);
+        });
 });
 
 
-app.get("/datalist/:id", function(req, res){
-    var id = req.params.id;
-    console.log(id);
-    db.datalist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
-        res.json(doc);
-    });
-});
-
-app.delete("/datalist/:id", function(req, res){
-    var id = req.params.id;
-    user.findByIdAndRemove({_id: id},
-    function(err, docs){
-        res.json(doc);
-    });
-});
-
-
-app.put("/datalist/:id", function(req, res){
+app.put("/datalist/:id", function(req, res) {
     var id = req.params.id;
     console.log(req.body.order);
-    datalist.findByIdAndUpdate({_id: id},
-    {
+    datalist.findByIdAndUpdate({
+        _id: id
+    }, {
         order: req.body.order,
         ordernumber: req.body.ordernumber,
         driver: req.body.driver,
@@ -114,7 +104,16 @@ app.put("/datalist/:id", function(req, res){
         pickup: req.body.pickup,
         destination: req.body.destination,
         status: req.body.status
-    }, function(err, doc){
+    }, function(err, doc) {
+        res.json(doc);
+    });
+});
+
+app.get("datalist/:id", function(res, req) {
+    var id = req.params.id;
+    datalist.findById({
+        _id: id
+    }, function(err, doc) {
         res.json(doc);
     });
 });
@@ -132,6 +131,34 @@ app.put("/datalist/:id", function(req, res){
     new: true}, function(err, doc){
         res.json(doc);
       });
+});
+
+
+app.delete("/datalist/:id", function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.datalist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
+});
+
+
+
+app.get("/datalist", function(req, res) {
+    console.log("I received a GET request");
+    db.datalist.find(function(err, docs){
+        console.log(docs);
+        res.json(docs);
+    });
+});
+
+
+app.get("/datalist/:id", function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.datalist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
 });
 
 */
